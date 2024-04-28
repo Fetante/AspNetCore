7b6a700495140dd70cfacf79a4b5e29186b04006
Fetante <andreas.carlssonmunkedal@gmail.com>
2024-04-29 00:10:35 +0200

    sign up completed

---
+++ AspNetCoreMVCSigIn/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AspNetCoreMVCSigIn.Models;
+
+namespace AspNetCoreMVCSigIn.ViewModels;
+
+public class SignUpViewModel
+{
+    public string Title { get; set; } = "Sign Up";
+    public SignUpModel Form { get; set; } = new SignUpModel();
+    
+}
