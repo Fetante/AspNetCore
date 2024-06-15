6a4b5a282f72a68fba761918523a896bd7e190cf
Fetante <andreas.carlssonmunkedal@gmail.com>
2024-06-15 16:04:01 +0200

    Created User and Addressrepos

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository(DataContext context) : Repo<UserEntity>(context)
+{
+    private readonly DataContext _context = context;
+}
